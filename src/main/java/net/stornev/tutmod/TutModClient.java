3eab0ecd51f095717927a21a396623a0679d33f4
Stornev <tjrobin33@gmail.com>
2022-10-10 18:05:52 -0400

    Initial commit

---
+++ src/main/java/net/stornev/tutmod/TutModClient.java
@@ -0,0 +1,10 @@
+package net.stornev.tutmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
